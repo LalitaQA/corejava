5666e376668578edc7c4792e88129a311f58c878
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-11 10:54:25 +0530

    push changes

--- src/ch8_objects/O_09_ObjectForNonStaticMethodInAnotherClass.java
+++ src/ch8_objects/O_09_ObjectForNonStaticMethodInAnotherClass.java
@@ -1,6 +1,6 @@
 package ch8_objects;
 
-class employee {
+class Employee {
 	// non-static method
 	public void data() {
 		System.out.println("Employee details");
@@ -11,7 +11,8 @@ class employee {
 public class O_09_ObjectForNonStaticMethodInAnotherClass {
 
 	public static void main(String[] args) {
-
+		// create an object
+		Employee ref = new Employee();
+		ref.data();
 	}
-
 }
