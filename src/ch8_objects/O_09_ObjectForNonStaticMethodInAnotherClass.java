978c150465fac45a4b5403fea630fb9745fd6e04
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-11 10:49:35 +0530

    push changes

--- src/ch8_objects/O_09_ObjectForNonStaticMethodInAnotherClass.java
+++ src/ch8_objects/O_09_ObjectForNonStaticMethodInAnotherClass.java
@@ -1,9 +1,17 @@
 package ch8_objects;
 
+class employee {
+	// non-static method
+	public void data() {
+		System.out.println("Employee details");
+
+	}
+}
+
 public class O_09_ObjectForNonStaticMethodInAnotherClass {
 
 	public static void main(String[] args) {
-		
+
 	}
 
 }
