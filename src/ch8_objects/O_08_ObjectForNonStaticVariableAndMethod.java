dc5cd745d3e286017cf8343d5c39325739c3b894
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-11 10:44:03 +0530

    push changes

--- src/ch8_objects/O_08_ObjectForNonStaticVariableAndMethod.java
+++ src/ch8_objects/O_08_ObjectForNonStaticVariableAndMethod.java
@@ -1,20 +1,26 @@
 package ch8_objects;
 
 public class O_08_ObjectForNonStaticVariableAndMethod {
-	//non-static variables
+	// non-static variables
 	String name;
 	double salary;
-	
-	//non-static method
-	public void details()
-	{
+
+	// non-static method
+	public void details() {
 		System.out.println("Employee data");
-		System.out.println("name :" +name);
-		System.out.println("salary :" +salary);
+		System.out.println("name :" + name);
+		System.out.println("salary :" + salary);
 	}
 
 	public static void main(String[] args) {
-		
-	}
+		System.out.println("Main method starts");
+		O_08_ObjectForNonStaticVariableAndMethod ref = new O_08_ObjectForNonStaticVariableAndMethod();
 
+		// initializing non-static variables
+		ref.name = "Jimmy";
+		ref.salary = 98000;
+
+		// fetching/print non-static variables and method
+		ref.details();
+	}
 }
