fe6f7a196f6be03ea3e92ae88cc23e2b3411eb70
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-11 10:40:15 +0530

    push changes

--- src/ch8_objects/O_08_ObjectForNonStaticVariableAndMethod.java
+++ src/ch8_objects/O_08_ObjectForNonStaticVariableAndMethod.java
@@ -8,7 +8,9 @@ public class O_08_ObjectForNonStaticVariableAndMethod {
 	//non-static method
 	public void details()
 	{
-		
+		System.out.println("Employee data");
+		System.out.println("name :" +name);
+		System.out.println("salary :" +salary);
 	}
 
 	public static void main(String[] args) {
