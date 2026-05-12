18cb5b2a9993449bc959a925917f618787e9190e
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-12 11:41:33 +0530

    push changes

--- src/ch8_objects/O_12_ObjectForStaticMethod.java
+++ src/ch8_objects/O_12_ObjectForStaticMethod.java
@@ -1,9 +1,18 @@
 package ch8_objects;
 
 public class O_12_ObjectForStaticMethod {
-
-	public static void main(String[] args) {
-		
+	// static method
+	public static void clgInfo() {
+		System.out.println("All the college details");
 	}
 
+	public static void main(String[] args) {
+		// accessing static method
+		clgInfo();// directly by using method signature
+
+		O_12_ObjectForStaticMethod.clgInfo();// using class name as reference
+
+		O_12_ObjectForStaticMethod ref = new O_12_ObjectForStaticMethod();
+		ref.clgInfo();// using object ref
+	}
 }
