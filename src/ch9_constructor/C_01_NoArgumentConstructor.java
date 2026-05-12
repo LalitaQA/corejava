a38da56fdd7a008ec2b9cce9bcef03339b3666ce
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-12 11:47:58 +0530

    push changes

--- src/ch9_constructor/C_01_NoArgumentConstructor.java
+++ src/ch9_constructor/C_01_NoArgumentConstructor.java
@@ -1,9 +1,16 @@
 package ch9_constructor;
 
 public class C_01_NoArgumentConstructor {
-
-	public static void main(String[] args) {
-		
+	// no-arg constructor
+	public C_01_NoArgumentConstructor() {
+		System.out.println("No-arg constructor");
 	}
 
+	public static void main(String[] args) {
+		System.out.println("Main method starts");
+		new C_01_NoArgumentConstructor();
+
+		C_01_NoArgumentConstructor arg = new C_01_NoArgumentConstructor();
+		System.out.println("Main method ends");
+	}
 }
