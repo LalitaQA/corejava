cc1cbbcdd1d82663b0f37a71ada43544dc730c49
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-13 11:15:01 +0530

    push changes

--- src/ch9_constructor/C_07_ParameterizedConstructorWithDiffNameNonStaticVariable.java
+++ src/ch9_constructor/C_07_ParameterizedConstructorWithDiffNameNonStaticVariable.java
@@ -1,9 +1,18 @@
 package ch9_constructor;
 
 public class C_07_ParameterizedConstructorWithDiffNameNonStaticVariable {
+	// non-static variable
+	int n = 4;
 
-	public static void main(String[] args) {
-		
+	C_07_ParameterizedConstructorWithDiffNameNonStaticVariable(int i)// i is local variable
+	{
+		System.out.println("i : " + i);
+		// when there is no local variable with similar to non-static variable
+		System.out.println("n : " + n);// this.n -implicitly
+		System.out.println("n : " + this.n);// explicitly
 	}
 
+	public static void main(String[] args) {
+		new C_07_ParameterizedConstructorWithDiffNameNonStaticVariable(10);
+	}
 }
