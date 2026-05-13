a0a1e22d305020f6eae068755d134663f16e6f0b
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-13 10:40:27 +0530

    push changes

--- src/ch9_constructor/C_04_MultipleParameterizedConstructor.java
+++ src/ch9_constructor/C_04_MultipleParameterizedConstructor.java
@@ -1,9 +1,14 @@
 package ch9_constructor;
 
 public class C_04_MultipleParameterizedConstructor {
-
-	public static void main(String[] args) {
-		
+	// parameterized constructor
+	public C_04_MultipleParameterizedConstructor(String name, double salary) {
+		System.out.println("Name : " + name);
+		System.out.println("Salaray : " + salary);
 	}
 
+	public static void main(String[] args) {
+		System.out.println("Main method start");
+		new C_04_MultipleParameterizedConstructor("ABC company", 9086688.000);
+	}
 }
