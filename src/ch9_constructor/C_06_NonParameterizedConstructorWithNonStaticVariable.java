dd846775d3f520cef02d10d9f65aac65b5320c18
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-13 11:29:51 +0530

    push changes

--- src/ch9_constructor/C_06_NonParameterizedConstructorWithNonStaticVariable.java
+++ src/ch9_constructor/C_06_NonParameterizedConstructorWithNonStaticVariable.java
@@ -1,15 +1,15 @@
 package ch9_constructor;
 
-public class C_06_ParameterizedConstructorWithSameNameNonStaticVariable {
+public class C_06_NonParameterizedConstructorWithNonStaticVariable {
 	// non-static variables
 	int n = 4;
 
-	C_06_ParameterizedConstructorWithSameNameNonStaticVariable() {
+	C_06_NonParameterizedConstructorWithNonStaticVariable() {
 		System.out.println("n : " + n);// this.n -implicitly (where there is no local variable)
 		System.out.println("n : " + this.n);// explicit
 	}
 
 	public static void main(String[] args) {
-		new C_06_ParameterizedConstructorWithSameNameNonStaticVariable();
+		new C_06_NonParameterizedConstructorWithNonStaticVariable();
 	}
 }
