1fd893634ae070a67b3f479d85f820d3f03e6794
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-13 11:10:03 +0530

    push changes

--- src/ch9_constructor/C_07_ParameterizedConstructorWithDiffNameNonStaticVariable.java
+++ src/ch9_constructor/C_07_ParameterizedConstructorWithDiffNameNonStaticVariable.java
@@ -3,8 +3,7 @@ package ch9_constructor;
 public class C_07_ParameterizedConstructorWithDiffNameNonStaticVariable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
