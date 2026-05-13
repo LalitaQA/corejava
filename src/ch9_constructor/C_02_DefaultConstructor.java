87b18e015cba62fda4caf9c365e5fbe3b9215c91
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-13 10:26:19 +0530

    push changes

--- src/ch9_constructor/C_02_DefaultConstructor.java
+++ src/ch9_constructor/C_02_DefaultConstructor.java
@@ -3,7 +3,10 @@ package ch9_constructor;
 public class C_02_DefaultConstructor {
 
 	public static void main(String[] args) {
-		
-	}
+		System.out.println("Main method start");
 
+		// default constructor call implicit by compiler
+
+		System.out.println("Main method end");
+	}
 }
