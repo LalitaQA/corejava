913abc50a3a2a125a0e7e0adda0bb1aed969acf1
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-13 10:46:05 +0530

    push changes

---
+++ src/ch9_constructor/C_05_ParameterizedConstructorWithNonStaticVariable.java
@@ -0,0 +1,10 @@
+package ch9_constructor;
+
+public class C_05_ParameterizedConstructorWithNonStaticVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
