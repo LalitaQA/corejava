98ddb38719a2a382bf977a229f37d2b4539a3165
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-13 10:34:46 +0530

    push changes

---
+++ src/ch9_constructor/C_03_SingleParameterizedConstructor.java
@@ -0,0 +1,14 @@
+package ch9_constructor;
+
+public class C_03_SingleParameterizedConstructor {
+	// constructor has same name as class
+	public C_03_SingleParameterizedConstructor(String name) {
+		System.out.println("Name :" + name);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Main method start");
+		new C_03_SingleParameterizedConstructor("Frill");
+		new C_03_SingleParameterizedConstructor("Dhruv");
+	}
+}
---
+++ src/ch9_constructor/C_04_MultipleParameterizedConstructor.java
@@ -0,0 +1,10 @@
+package ch9_constructor;
+
+public class C_04_MultipleParameterizedConstructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
