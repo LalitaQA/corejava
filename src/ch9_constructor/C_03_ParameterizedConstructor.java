f3ff1523bac1269e92b88a0d5ac26e72d11f4016
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-13 10:32:41 +0530

    push changes

--- src/ch9_constructor/C_03_ParameterizedConstructor.java
+++ src/ch9_constructor/C_03_ParameterizedConstructor.java
@@ -1,9 +1,14 @@
 package ch9_constructor;
 
 public class C_03_ParameterizedConstructor {
-
-	public static void main(String[] args) {
-		
+	// constructor has same name as class
+	public C_03_ParameterizedConstructor(String name) {
+		System.out.println("Name :" + name);
 	}
 
+	public static void main(String[] args) {
+		System.out.println("Main method start");
+		new C_03_ParameterizedConstructor("Frill");
+		new C_03_ParameterizedConstructor("Dhruv");
+	}
 }
