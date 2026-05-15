8a59bd2569a9c5c73b786e7e5df4a049d7597774
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-15 10:47:12 +0530

    push changes

--- src/ch10_constructoroverloading/CO_01BasedOnLength.java
+++ src/ch10_constructoroverloading/CO_01BasedOnLength.java
@@ -1,9 +1,19 @@
 package ch10_constructoroverloading;
 
 public class CO_01BasedOnLength {
-
-	public static void main(String[] args) {
-		
+	// constructor without parameter
+	CO_01BasedOnLength() {
+		System.out.println("No arg constructor");
 	}
 
+	// constructor with parameter
+	CO_01BasedOnLength(int n) {
+		System.out.println("Parameterized constructor");
+		System.out.println("n : " + n);
+	}
+
+	public static void main(String[] args) {
+		new CO_01BasedOnLength();
+		new CO_01BasedOnLength(5);
+	}
 }
