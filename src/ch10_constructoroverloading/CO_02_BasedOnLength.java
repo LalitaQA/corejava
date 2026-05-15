6d210af5b7cc46d35ff0116ca33323a37389310d
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-15 11:08:34 +0530

    push changes

--- src/ch10_constructoroverloading/CO_02_BasedOnLength.java
+++ src/ch10_constructoroverloading/CO_02_BasedOnLength.java
@@ -1,19 +1,20 @@
 package ch10_constructoroverloading;
 
 public class CO_02_BasedOnLength {
-	// constructor without parameter
-	CO_02_BasedOnLength() {
-		System.out.println("No arg constructor");
+	// constructor with 1 parameter
+	CO_02_BasedOnLength(String name) {
+		System.out.println("Name : " + name);
 	}
 
-	// constructor with parameter
-	CO_02_BasedOnLength(int n) {
-		System.out.println("Parameterized constructor");
-		System.out.println("n : " + n);
+	// constructor with 2 parameter
+	CO_02_BasedOnLength(String name, int age) {
+		System.out.println("Name :" + name);
+		System.out.println("Age : " + age);
 	}
 
 	public static void main(String[] args) {
-		new CO_02_BasedOnLength();
-		new CO_02_BasedOnLength(5);
+		new CO_02_BasedOnLength("Hari", 20);
+		// new CO_02_BasedOnLength(5);
+		new CO_02_BasedOnLength("Jacob");
 	}
 }
