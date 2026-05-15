8a64159b1da476e6e50e93252355ddaa2333fb5f
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-15 11:33:01 +0530

    push changes

--- src/ch10_constructoroverloading/CO_04_BasedOnOrderOfOccurence.java
+++ src/ch10_constructoroverloading/CO_04_BasedOnOrderOfOccurence.java
@@ -1,11 +1,19 @@
 package ch10_constructoroverloading;
 
 public class CO_04_BasedOnOrderOfOccurence {
-	//overloading constructor based on Order Of Occurence of datatypes
-	
-
-	public static void main(String[] args) {
-		
+	// overloading constructor based on Order Of Occurence of datatypes
+	CO_04_BasedOnOrderOfOccurence(String brand, double price) {
+		System.out.println("Brand :" + brand);
+		System.out.println("Price :" + price);
 	}
 
+	CO_04_BasedOnOrderOfOccurence(double price, String brand) {
+		System.out.println("Brand :" + brand);
+		System.out.println("Price :" + price);
+	}
+
+	public static void main(String[] args) {
+		new CO_04_BasedOnOrderOfOccurence(900000, "Asus");
+		new CO_04_BasedOnOrderOfOccurence("HCL", 800000);
+	}
 }
