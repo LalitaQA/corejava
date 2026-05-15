579fbbbe49df4f93f5a335ee6980b5e57e620d09
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-15 11:04:12 +0530

    push changes

--- src/ch10_constructoroverloading/CO_01_BasedOnPrameterizedAndNonParameterized.java
+++ src/ch10_constructoroverloading/CO_01_BasedOnPrameterizedAndNonParameterized.java
@@ -1,9 +1,19 @@
 package ch10_constructoroverloading;
 
 public class CO_01_BasedOnPrameterizedAndNonParameterized {
-
-	public static void main(String[] args) {
-		
+	// constructor without parameter
+	CO_01_BasedOnPrameterizedAndNonParameterized() {
+		System.out.println("No arg constructor");
 	}
 
+	// constructor with parameter
+	CO_01_BasedOnPrameterizedAndNonParameterized(int n) {
+		System.out.println("Parameterized constructor");
+		System.out.println("n : " + n);
+	}
+
+	public static void main(String[] args) {
+		new CO_02_BasedOnLength();
+		new CO_02_BasedOnLength(5);
+	}
 }
