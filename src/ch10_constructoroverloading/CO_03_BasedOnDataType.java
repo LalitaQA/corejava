9588254653425ebaf1d9dc5772edf95a894d1dd7
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-15 11:22:00 +0530

    push changes

--- src/ch10_constructoroverloading/CO_03_BasedOnDataType.java
+++ src/ch10_constructoroverloading/CO_03_BasedOnDataType.java
@@ -2,8 +2,18 @@ package ch10_constructoroverloading;
 
 public class CO_03_BasedOnDataType {
 
-	public static void main(String[] args) {
-		
+	CO_03_BasedOnDataType(String name, long phno) {
+		System.out.println("Name :" + name);
+		System.out.println("Phone :" + phno);
 	}
 
+	CO_03_BasedOnDataType(int rno, long phno) {
+		System.out.println("Roll no :" + rno);
+		System.out.println("Phone :" + phno);
+	}
+
+	public static void main(String[] args) {
+		new CO_03_BasedOnDataType(11, 56898900088L);
+		new CO_03_BasedOnDataType("Harshita", 967277238328L);
+	}
 }
